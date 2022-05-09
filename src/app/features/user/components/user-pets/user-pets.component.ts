d94d2b437dfcd47824e56f53f7b698b33ebf2dd9
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2022-05-08 22:52:47 -0300

    refactor: funcao para remover pets modificada para atender mudanca na service

--- src/app/features/user/components/user-pets/user-pets.component.ts
+++ src/app/features/user/components/user-pets/user-pets.component.ts
@@ -7,51 +7,46 @@ import { UserService } from 'src/app/features/services/user.service';
 @Component({
   selector: 'app-user-pets',
   templateUrl: './user-pets.component.html',
-  styleUrls: ['./user-pets.component.scss']
+  styleUrls: ['./user-pets.component.scss'],
 })
 export class UserPetsComponent implements OnInit {
-
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private petService: PetService,
     private router: Router
-    
-    ) { }
+  ) {}
 
-    @Input()
-    pet!: PetModel;  
-    userId!: string;
-    pets!: PetModel[];
-    idPet!: number
-    detailsPet!: PetModel
-
+  @Input()
+  pet!: PetModel;
+  userId!: string;
+  pets!: PetModel[];
+  idPet!: number;
+  detailsPet!: PetModel;
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.userId = params['id']
-       });
-    
-     
+      this.userId = params['id'];
+    });
+  }
+  
+  deletePet() {
+    this.petService
+      .deletePet(parseInt(this.userId), this.idPet)
+      .subscribe((result) => {
+        result;
+      });
+    location.reload();
   }
 
-
-  deletePet(){
-    this.petService.deletePet(parseInt(this.userId), this.idPet).subscribe();
+  getById() {
+    this.idPet = this.pet.id;
+    console.log(this.idPet);
   }
 
-  getById(){
-   this.idPet = this.pet.id
-   console.log(this.idPet)
+  navigateDetails() {
+    this.idPet = this.pet.id;
+
+    this.router.navigateByUrl(`user/${parseInt(this.userId)}/${this.idPet}`);
   }
-
-  navigateDetails(){
-
-     this.idPet = this.pet.id
-
-    this.router.navigateByUrl(`user/${parseInt(this.userId)}/${this.idPet}`)
-    
-  }
-
-
 }
